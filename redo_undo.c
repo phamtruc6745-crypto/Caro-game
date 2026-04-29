b0329ac198a9747a39d8362bdda834cc1c0add27
HackerDeathFake <phamminhphuclop41@gmail.com>
2026-04-29 11:31:34 +0700

    Implement header guard and remove stack logic
    Added header guard to prevent multiple inclusions and removed stack implementation for move history management.

--- redo_undo.c
+++ redo_undo.c
@@ -1,84 +1,10 @@
-#include <stdbool.h>
+#ifndef LOGIC_GAME_H
+#define LOGIC_GAME_H
 
-// ===== KIEU DU LIEU =====
-typedef enum {
-    NA = 0,
-    BLACK = 1,
-    WHITE = 2
-} Stone;
+#include "cau_truc_du_lieu.h"
 
-typedef struct {
-    int row;
-    int col;
-    Stone player;
-} Move;
+int dem_huong(char ban_co[KICH_THUOC_TOI_DA][KICH_THUOC_TOI_DA], int kich_thuoc, int hang, int cot, int delta_hang, int delta_cot, char nguoi_choi);
+bool kiem_tra_thang(char ban_co[KICH_THUOC_TOI_DA][KICH_THUOC_TOI_DA], int kich_thuoc, int hang_cuoi, int cot_cuoi, char nguoi_choi, int so_quan_thang);
+bool thuc_hien_hoan_tac(TrangThaiVanCo *van_co, int *hang_con_tro, int *cot_con_tro);
 
-#define MAX_MOVES 100
-
-typedef struct {
-    Move data[MAX_MOVES];
-    int top;
-} Stack;
-
-// ===== STACK CO BAN =====
-void initStack(Stack *s) {
-    s->top = -1;
-}
-
-bool isEmpty(Stack *s) {
-    return s->top == -1;
-}
-
-bool isFull(Stack *s) {
-    return s->top == MAX_MOVES - 1;
-}
-
-// pushMove
-bool pushMove(Stack *s, Move m) {
-    if (isFull(s)) return false;
-    s->data[++s->top] = m;
-    return true;
-}
-
-// popMove
-bool popMove(Stack *s, Move *m) {
-    if (isEmpty(s)) return false;
-    *m = s->data[s->top--];
-    return true;
-}
-
-// ===== UNDO =====
-// xoa nuoc cuoi tren board va dua sang redo
-bool undo(Stack *history, Stack *redoStack,
-          Stone board[][50], Stone *currentPlayer, int *moves) {
-    
-    Move m;
-    if (!popMove(history, &m)) return false;
-
-    board[m.row][m.col] = NA;
-
-    pushMove(redoStack, m);
-
-    *currentPlayer = m.player;
-    (*moves)--;
-
-    return true;
-}
-
-// ===== REDO =====
-// dat lai nuoc vua undo
-bool redo(Stack *history, Stack *redoStack,
-          Stone board[][50], Stone *currentPlayer, int *moves) {
-    
-    Move m;
-    if (!popMove(redoStack, &m)) return false;
-
-    board[m.row][m.col] = m.player;
-
-    pushMove(history, m);
-
-    *currentPlayer = (m.player == BLACK) ? WHITE : BLACK;
-    (*moves)++;
-
-    return true;
-}
+#endif
