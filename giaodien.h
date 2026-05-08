314d4205601f6b46ff02650f42be2ef9a6b84320
Thanh Truc <phamtruc6745@gmail.com>
2026-05-08 15:15:46 +0700

    Update header file giaodien.h with new includes and functions

--- giaodien.h
+++ giaodien.h
@@ -1,18 +1,15 @@
 #ifndef GIAODIEN_H
 #define GIAODIEN_H
 
-#include "cautruc.h"
+#include "cau_truc_du_lieu (1).h"
 #include <stdbool.h>
 
-// Toa do dong bat dau cua hang 0 tren ban co (duoc do dong sau moi lan ve)
-// Dung thay cho TOA_DO_Y_BAT_DAU de tranh loi lech do man hinh scroll
-extern int g_hangBatDauBanCo;
-extern int g_toaDoY_Prompt;
-
+void gotoXY(int x, int y);
 void xoaManHinh();
-void veBanCo(BanCo* banCo, int conTroX, int conTroY, NuocDi nuocCuoi, bool veDayDu);
+void inHieuUngTieuDe();
+void veBanCo(BanCo* banCo, int conTroX, int conTroY, NuocDi nuocCuoi, const char* tenHienTai, NguoiChoiHienTai luot);
 void inMenuChinh();
-void inMenuKichThuoc();
+void inMenuChonKichThuoc();
 void inThongBao(const char* thongBao);
 
 #endif // GIAODIEN_H
