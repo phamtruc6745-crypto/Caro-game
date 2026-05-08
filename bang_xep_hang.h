ad1c56a3716fb14bb99fff0883f0e95f27b0660d
Sơn <ledinhson070504@gmail.com>
2026-05-08 15:15:19 +0700

    Update bang_xep_hang.h

--- bang_xep_hang.h
+++ bang_xep_hang.h
@@ -3,6 +3,9 @@
 
 #include "cau_truc_du_lieu.h"
 
-void cap_nhat_ket_qua(const char *t1, const char *t2, const char *p1, const char *p2, int hoa);
-void hien_thi_bang_xep_hang(void);
-#endif
+#define TEP_BANG_XEP_HANG "bangxephang.txt"
+
+void capNhatBangXepHang(ThongTinNguoiChoi ng);
+void hienThiBangXepHang();
+
+#endif // BANG_XEP_HANG_H
