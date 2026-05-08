955380cec3ecd4f715cb2a1b6eac24ddb0638b99
HackerDeathFake <phamminhphuclop41@gmail.com>
2026-05-08 17:08:26 +0700

    Rename header guard and update function declarations

--- logic_game.h
+++ logic_game.h
@@ -1,10 +1,13 @@
-#ifndef LOGIC_GAME_H
-#define LOGIC_GAME_H
+#ifndef LUATCHOI_H
+#define LUATCHOI_H
 
 #include "cau_truc_du_lieu.h"
+#include <stdbool.h>
 
-int dem_huong(char ban_co[KICH_THUOC_TOI_DA][KICH_THUOC_TOI_DA], int kich_thuoc, int hang, int cot, int delta_hang, int delta_cot, char nguoi_choi);
-bool kiem_tra_thang(char ban_co[KICH_THUOC_TOI_DA][KICH_THUOC_TOI_DA], int kich_thuoc, int hang_cuoi, int cot_cuoi, char nguoi_choi, int so_quan_thang);
-bool thuc_hien_hoan_tac(TrangThaiVanCo *van_co, int *hang_con_tro, int *cot_con_tro);
+void khoiTaoBanCo(BanCo* banCo, int kichThuoc, int dkThang);
+bool nuocDiHopLe(BanCo* banCo, int x, int y);
+void danhCo(BanCo* banCo, NuocDi nuoc);
+void hoanTacNuocDi(BanCo* banCo, NuocDi nuoc);
+TrangThaiGame kiemTraThangThua(BanCo* banCo, NuocDi nuocCuoi);
 
-#endif
+#endif // LUATCHOI_H
